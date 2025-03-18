e4095c9325a98ddb15d9fffcfc4a0890417c8c77
5aaee9 <7685264+5aaee9@users.noreply.github.com>
2025-03-18 20:01:09 +0800

    fix: zsh custom path

--- scripts/20-setup-zsh.sh
+++ scripts/20-setup-zsh.sh
@@ -5,7 +5,7 @@ set -ex
 chsh -s /usr/bin/zsh
 echo "" | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-export ZSH_CUSTOM=/root/.zsh/custom
+export ZSH_CUSTOM=/root/.oh-my-zsh/custom
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
