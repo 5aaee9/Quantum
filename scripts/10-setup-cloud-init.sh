80351b36af93367347e19cecf5620651d14dd6ab
5aaee9 <7685264+5aaee9@users.noreply.github.com>
2025-03-18 16:32:23 +0800

    feat: update cloud init conf

--- scripts/10-setup-cloud-init.sh
+++ scripts/10-setup-cloud-init.sh
@@ -8,3 +8,25 @@ systemctl enable qemu-guest-agent
 apt-get install cloud-guest-utils cloud-init -y
 
 systemctl add-wants multi-user.target cloud-init.target
+
+mkdir -p /etc/cloud/cloud.cfg.d
+
+cat > /etc/cloud/cloud.cfg.d/01_debian_cloud.cfg <<EOF
+datasource_list:
+- NoCloud
+- ConfigDrive
+
+apt:
+  generate_mirrorlists: true
+
+system_info:
+  default_user:
+    name: debian
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    shell: /bin/zsh
+    lock_passwd: True
+    gecos: Debian
+    groups: [adm, audio, cdrom, dialout, dip, floppy, plugdev, sudo, video]
+    sudo: ["ALL=(ALL) NOPASSWD:ALL"]
+    shell: /bin/zsh
+EOF
